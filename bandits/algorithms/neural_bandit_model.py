409a318aaf4b503d68e9d469032696fb32fe44a9
Andrew Kondrich <andrewk1@stanford.edu>
2019-08-08 17:40:17 -0700

    change to consistent hparam accessor

--- bandits/algorithms/neural_bandit_model.py
+++ bandits/algorithms/neural_bandit_model.py
@@ -27,23 +27,23 @@ class NeuralBanditModel():
 
     def build_model(self):
         self.net = []
-        input_dim = self.hparams['context_dim']
+        input_dim = self.hparams.context_dim
 
         for output_dim in self.hparams["layer_sizes"]:
             self.net += self.build_layer(input_dim, output_dim)
             input_dim = output_dim
 
-        self.net += self.build_layer(input_dim, self.hparams["num_actions"])
+        self.net += self.build_layer(input_dim, self.hparams.num_actions)
         self.net = nn.Sequential(*self.net)
         self.lossCriterion = nn.MSELoss(reduction='none')
         self.optimizer = optim.RMSprop(self.net.parameters(),
-                                       lr=self.hparams["initial_lr"])
+                                       lr=self.hparams.initial_lr)
         self.assign_lr()
 
     def assign_lr(self):
         # TODO: This isn't 1-1 the same
         self.scheduler = optim.lr_scheduler.StepLR(self.optimizer,
-                                                   self.hparams['global_step'])
+                                                   self.hparams.global_step)
 
     def train(self, data, num_steps):
         print("Training {} for {} steps...".format(self.name, num_steps))
@@ -52,13 +52,13 @@ class NeuralBanditModel():
             self.optimizer.zero_grad()
 
             # Get data, perform forward prop
-            x, y, w = data.get_batch_with_weights(self.hparams["batch_size"])
+            x, y, w = data.get_batch_with_weights(self.hparams.batch_size)
             y_pred = self.net.forward(x)
 
             # Compute loss
             loss = self.lossCriterion(y_pred, y)
             loss = loss * w  # This pulls out loss only on true y
-            cost = loss.mean() / self.hparams["batch_size"]
+            cost = loss.mean() / self.hparams.batch_size
             cost.backward()
             self.optimizer.step()
 
